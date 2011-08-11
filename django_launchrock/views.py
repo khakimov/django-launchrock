d6c7f94c8328363ff7886b1565c8d6f47da64fb6
Pupsor <pupsor@gmail.com>
2011-08-11 12:57:45 -0700

    change form. Displaying a form using a form.as_p

--- django_launchrock/views.py
+++ django_launchrock/views.py
@@ -3,16 +3,30 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from launch.forms import LaunchRockForm
 from launch.models import LaunchRock
+from django.forms import ModelForm
+from django import forms
 
 import datetime
+
+class LaunchRockForm(ModelForm):
+    class Meta:
+        model = LaunchRock
+        exclude = ['sign_date', 'ip', 'http_refer']
+
+    def clean_email(self):
+        try:
+            LaunchRock.objects.get(email=self.cleaned_data['email'])
+        except LaunchRock.DoesNotExist:
+            return self.cleaned_data['email']
+        raise forms.ValidationError("This is email is already in database.")
+
 
 def signup(request):
     if request.method == 'POST':
         form = LaunchRockForm(data=request.POST)
         if form.is_valid():
-            launch = LaunchRock(email=form.cleaned_data['email'], sign_date=datetime.datetime.now(), 
+            launch = LaunchRock.objects.create(email=form.cleaned_data['email'], sign_date=datetime.datetime.now(), 
                                     ip=request.META['REMOTE_ADDR'], http_refer = request.META['HTTP_REFERER'])
-            launch.save()
             return HttpResponseRedirect("/done/")
     else:
         form = LaunchRockForm()
